5ee45af643061c5cc55fd77deb185cf69e959239
oaadmin@protonmail.com <oaadmin@protonmail.com>
2023-06-06 03:16:23 +0000

    Edited methods

--- client_code/methods/__init__.py
+++ client_code/methods/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import methodsTemplate
 from anvil import *
 from anvil.js import import_from
+import json
 StandardMerkleTree = import_from("@openzeppelin/merkle-tree").StandardMerkleTree
 class methods(methodsTemplate):
   def __init__(self, **properties):
@@ -9,7 +10,7 @@ class methods(methodsTemplate):
   def build_merkle(self, data, format):
     tree = StandardMerkleTree.of(data, format)
     root = tree.root
-    proofs = [{"proof":tree.getProof(data.index(r)), "address":r[0], "merkle_value":r[1]} for r in data]
+    proofs = [{"proof":json.dumps(tree.getProof(data.index(r))), "address":r[0], "merkle_value":r[1]} for r in data]
     return tree, root, proofs
 
     # Any code you write here will run before the form opens.
