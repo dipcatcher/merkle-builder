7d201b4291f027c1bac34f6f3b01cd58c5ed0a95
oaadmin@protonmail.com <oaadmin@protonmail.com>
2024-02-17 21:17:30 +0000

    Edited methods

--- client_code/methods/__init__.py
+++ client_code/methods/__init__.py
@@ -8,10 +8,23 @@ class methods(methodsTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
   def build_merkle(self, data, format, identifier, value):
-    '''pass data and format, name of fields'''
+    """
+    Builds a Merkle tree from the provided data and format, and generates proofs for each data entry.
+    
+    Parameters:
+    - data: The dataset to be used in the Merkle tree.
+    - format: The format of the data. ["address", "uint256] if list of lists.
+    - identifier: The field name to be used as an identifier for each entry.
+    - value: The field name to be used as the value for each entry.
+    
+    Returns:
+    A tuple containing the Merkle tree, its root, and the proofs for each entry.
+    """
+    # Create the Merkle tree
     tree = StandardMerkleTree.of(data, format)
     root = tree.root
-    #proofs = [{"proof":json.dumps(tree.getProof(data.index(r))), identifier:r[0], value:r[1]} for r in data]
-    proofs = [{"proof":tree.getProof(data.index(r)), identifier:r[0], value:r[1]} for r in data]
+    
+    # Generate proofs for each entry in the dataset
+    proofs = [{"proof": tree.getProof(data.index(r)), identifier: r[0], value: r[1]} for r in data]
+    
     return tree, root, proofs
-  
